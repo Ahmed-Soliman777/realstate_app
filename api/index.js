b89a0d56baad3bfffdb7e9e71afe7c1c766a9b0b
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-17 19:05:41 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const port = 3000;
@@ -19,3 +20,5 @@ mongoose
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+app.use("/api/user", userRouter)
