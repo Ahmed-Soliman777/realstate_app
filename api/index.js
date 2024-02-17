337fbefee0a080c361e855bb4b12c4d2dd4f9786
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-17 18:28:39 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(new Date(), `MongoDB connected`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
