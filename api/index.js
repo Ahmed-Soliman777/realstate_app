30d51dc1c504c2a0b82fb3a703238f877acfee97
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-17 20:19:00 +0200

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 dotenv.config();
 
@@ -21,4 +24,5 @@ app.listen(port, () => {
   console.log("Listening on port " + port);
 });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
