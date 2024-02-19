962bca4cf6a6ed65989de55b9adce9ae3cbc23be
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-19 15:32:56 +0200

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 dotenv.config();
 
